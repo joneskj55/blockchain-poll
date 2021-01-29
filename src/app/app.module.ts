46957aaeb199067f0f9bbeaf98643ef6e3dfd91e
joneskj55 <joneskj55@gmail.com>
2021-01-29 13:14:30 -0600

    add poll-vote

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppComponent } from './app.component';
 import { PollCreateComponent } from './poll-create/poll-create.component';
 import { FormsModule, ReactiveFormsModule} from "@angular/forms";
 import { PollComponent } from './poll/poll.component';
+import { PollVoteComponent } from './poll-vote/poll-vote.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     PollCreateComponent,
-    PollComponent
+    PollComponent,
+    PollVoteComponent
   ],
   imports: [
     BrowserModule, FormsModule, ReactiveFormsModule
