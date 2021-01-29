fb220919511c365826a96465cfe253d9ebafc042
joneskj55 <joneskj55@gmail.com>
2021-01-29 14:27:12 -0600

    show chart

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import {Component} from '@angular/core';
+import {Poll} from './types';
 
 @Component({
   selector: 'app-root',
@@ -7,19 +8,32 @@ import {Component} from '@angular/core';
 })
 export class AppComponent {
   showForm = false;
+  activePoll: Poll = null;
 
-  polls = [
+  polls: Poll[] = [
     {
-      question: "Do you like dogs or cats?",
-      image: "assets/dig-dark.webp",
-      votes: [0, 5, 7],
+      id: 1,
+      question: 'Do you like dogs or cats?',
+      thumbnail: 'assets/dig-dark.webp',
+      results: [0, 5, 7],
+      options: ['cats', 'dogs', 'none'],
       voted: true,
     },
     {
-      question: "Best month?",
-      image: "assets/dig-dark.webp",
-      votes: [1, 5, 7],
+      id: 2,
+      question: 'Best month?',
+      thumbnail: 'assets/dig-dark.webp',
+      results: [1, 5, 7],
+      options: ['June', 'July', 'August'],
       voted: false,
     },
   ];
+
+  setActivePoll(poll) {
+    this.activePoll = null;
+
+    setTimeout(() => {
+      this.activePoll = poll;
+    }, 100);
+  }
 }
