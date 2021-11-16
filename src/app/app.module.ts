9962b3a5e0efcaac6a06988a5af4485e6263ec4b
joneskj55 <joneskj55@gmail.com>
2021-11-16 15:20:15 -0600

    add poll-create logic

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { PollCreateComponent } from './poll-create/poll-create.component';
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 
 @NgModule({
   declarations: [
@@ -10,7 +11,7 @@ import { PollCreateComponent } from './poll-create/poll-create.component';
     PollCreateComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, FormsModule, ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
