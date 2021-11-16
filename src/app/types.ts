1651685f4cdd9293a7711d27e4817aae3132b666
joneskj55 <joneskj55@gmail.com>
2021-11-16 14:25:32 -0600

    create Poll & Voter interfaces; add nav and jumbotron

---
+++ src/app/types.ts
@@ -0,0 +1,12 @@
+export interface Poll {
+  id: number; // 12
+  question: string; // Which days of the week do you like most?
+  results: number[]; // [0, 0, 0, 0, 4, 5, 7, 3]
+  options: string[]; // ["Mon", "Tue", "Wed" ....]
+  thumbnail: string; // https://image.png
+}
+
+export interface Voter {
+  id: string; // random hash
+  voted: number[]; // [12]
+}
