55513bb586e7f5883f1bbc70a23c106500bbae33
joneskj55 <joneskj55@gmail.com>
2021-11-17 11:09:51 -0600

    add poll component (logic); fix dependencies

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,19 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   showForm = false;
+
+  polls = [
+    {
+      question: 'Do you like dogs or cats?',
+      image: '../../assets/dog.jpg',
+      votes: [0, 5, 7],
+      voted: true,
+    },
+    {
+      question: 'Best month for summer holidays?',
+      image: '../../assets/beach.jpg',
+      votes: [1, 6, 4],
+      voted: false,
+    },
+  ];
 }
